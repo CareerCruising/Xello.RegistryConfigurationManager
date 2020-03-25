fdcddd4c94e129acb0951a1b832b311693afe6fa
swestner <swestner@gmail.com>
2020-03-24 20:54:25 -0400

    adding configuration extensions

---
+++ Xello.RegistryConfigurationManager/RegistryConfigurationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Win32;
+using System;
+
+namespace Xello.RegistryConfigurationManager
+{
+    public static class RegistryConfigurationExtensions
+    {
+        public static string DefaultSubKey { get; set; } = @"SOFTWARE\xello";
+
+        public static IConfigurationBuilder AddRegistry(this IConfigurationBuilder builder)
+        { 
+            
+            builder.AddRegistry(RegistryConfigurationExtensions.DefaultSubKey);
+            return builder;
+
+        }
+
+        public static IConfigurationBuilder AddRegistry(this IConfigurationBuilder builder, string path)
+        {
+            builder.Add(new RegistryConfigurationSource(path));
+            return builder;
+        }
+    }
+}
